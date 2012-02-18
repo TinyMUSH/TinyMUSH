a0484f6f3d0308d8332f753c5925cd07a2957ad5
Eddy Beaupré <Eddy Beaupr@Eddy-XPS>
2012-02-18 12:23:11 -0500

    Source Cleanup

--- game/include/version.h
+++
@@ -1,8 +0,0 @@
-#include "copyright.h"
-#ifndef __VERSION_H
-#define __VERSION_H
-#define MUSH_BUILD_DATE "Sun Jun 12 18:19:51 EDT 2011"
-#define MUSH_BUILD_NUM "3"
-#define MUSH_BUILD_COMPILE "gcc -DHAVE_CONFIG_H -I.  -I./libltdl -I./dbm/gdbm/     -g -O2"
-#define MUSH_BUILD_LTCOMPILE "/usr/local/bin/bash ./libtool --tag=CC    --mode=compile gcc -DHAVE_CONFIG_H -I.   -I./libltdl -I./dbm/gdbm/    -g -O2"
-#endif /* __VERSION_H */
