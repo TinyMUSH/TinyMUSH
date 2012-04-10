4176a3da83dbbdcd3f9b0eb42e448557bbc517f8
Eddy Beaupré <eddy@beaupre.biz>
2012-04-09 23:15:48 -0400

    Cleanup
    Cleanup...

--- src/tmp.sh
+++
@@ -1,3 +0,0 @@
-for i in alarm dup2 floor getdtablesize gethostbyaddr gethostbyname getpagesize getrusage gettimeofday inet_ntoa isascii memmove memset mktime pow select setrlimit socket sqrt srandom strcasecmp strchr strcspn strdup strerror strpbrk strrchr strspn strstr strtok_r strtoul timelocal tzset vsnprintf wait3 wait4; do \
-man $i
-done;
