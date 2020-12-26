73b1c625004653130796e44843abde4da8b5040a
Eddy Beaupré <eddy@beaupre.biz>
2020-12-26 13:26:53 -0500

    Testing Doxygen generation.

--- .ci/generateDocumentation.sh
+++ .ci/generateDocumentation.sh
@@ -50,7 +50,7 @@ git clone -b gh-pages git@github.com:$GH_REPO_ORG/$GH_REPO_NAME.git code_docs
 cd code_docs
 
 # Copy DoxygenLayout.xml here
-cp $TRAVIS_BUILD_DIR/doc/DoxygenLayout.xml .
+#cp $TRAVIS_BUILD_DIR/doc/DoxygenLayout.xml .
 
 ##### Configure git.
 # Set the push default to simple i.e. push only the current branch.
