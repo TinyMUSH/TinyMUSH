5a92f48e41dce272990dda645b1d097f9b8c53df
Eddy Beaupré <eddy@beaupre.biz>
2020-12-27 18:53:39 -0500

    Updating travis-ci

---
+++ .ci/dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
+    sudo apt-get -y install cproto libpcre3-dev libgdbm-dev doxygen graphviz
+elif [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
+    sudo brew install autoconf automake libtool cproto gdbm pcre
+fi
