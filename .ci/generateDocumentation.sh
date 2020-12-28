064c8aecb016d7861ce9a5f3837e992622fa5858
Eddy Beaupré <eddy@beaupre.biz>
2020-12-27 20:40:00 -0500

    Updating travis-ci

--- .ci/generateDocumentation.sh
+++ .ci/generateDocumentation.sh
@@ -34,82 +34,76 @@
 ################################################################################
 
 
-if [[ "$TRAVIS_BRANCH" = "master" && -n "$TRAVIS_BUILD_DOCS" && "$TRAVIS_PULL_REQUEST" = "false" ]] ; then
-    if [[ "$TRAVIS_OS_NAME" = "linux"]] ; then
-        if [[ "$TRAVIS_COMPILER" = "gcc" ]]; then
-            source .ci/setup-ssh.sh || travis_terminate 1
-            ################################################################################
-            ##### Setup this script and get the current gh-pages branch.               #####
-            echo 'Setting up the script...'
-            # Exit with nonzero exit code if anything fails
-            set -e
+if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_OS_NAME" = "linux" && "$TRAVIS_COMPILER" = "gcc" ]]; then
+    source .ci/setup-ssh.sh || travis_terminate 1
+    ################################################################################
+    ##### Setup this script and get the current gh-pages branch.               #####
+    echo 'Setting up the script...'
+    # Exit with nonzero exit code if anything fails
+    set -e
 
-            GH_REPO_ORG=`echo $TRAVIS_REPO_SLUG | cut -d "/" -f 1`
-            GH_REPO_NAME=`echo $TRAVIS_REPO_SLUG | cut -d "/" -f 2`
-            GH_REPO_REF="github.com/$GH_REPO_ORG/$GH_REPO_NAME.git"
-            DOXYFILE=$TRAVIS_BUILD_DIR/.ci/Doxyfile
+    GH_REPO_ORG=`echo $TRAVIS_REPO_SLUG | cut -d "/" -f 1`
+    GH_REPO_NAME=`echo $TRAVIS_REPO_SLUG | cut -d "/" -f 2`
+    GH_REPO_REF="github.com/$GH_REPO_ORG/$GH_REPO_NAME.git"
+    DOXYFILE=$TRAVIS_BUILD_DIR/.ci/Doxyfile
 
 
-            # Get the current gh-pages branch
-            git clone -b gh-pages git@github.com:$GH_REPO_ORG/$GH_REPO_NAME.git code_docs
-            cd code_docs
+    # Get the current gh-pages branch
+    git clone -b gh-pages git@github.com:$GH_REPO_ORG/$GH_REPO_NAME.git code_docs
+    cd code_docs
 
-            # Copy DoxygenLayout.xml here
-            #cp $TRAVIS_BUILD_DIR/doc/DoxygenLayout.xml .
+    # Copy DoxygenLayout.xml here
+    #cp $TRAVIS_BUILD_DIR/doc/DoxygenLayout.xml .
 
-            ##### Configure git.
-            # Set the push default to simple i.e. push only the current branch.
-            git config --global push.default simple
-            # Pretend to be an user called Travis CI.
-            git config user.name "Travis CI"
-            git config user.email "travis@travis-ci.org"
+    ##### Configure git.
+    # Set the push default to simple i.e. push only the current branch.
+    git config --global push.default simple
+    # Pretend to be an user called Travis CI.
+    git config user.name "Travis CI"
+    git config user.email "travis@travis-ci.org"
 
-            # go back to first commit
-            git reset --hard `git rev-list --max-parents=0 --abbrev-commit HEAD`
+    # go back to first commit
+    git reset --hard `git rev-list --max-parents=0 --abbrev-commit HEAD`
 
-            # Need to create a .nojekyll file to allow filenames starting with an underscore
-            # to be seen on the gh-pages site. Therefore creating an empty .nojekyll file.
-            # Presumably this is only needed when the SHORT_NAMES option in Doxygen is set
-            # to NO, which it is by default. So creating the file just in case.
-            echo "" > .nojekyll
+    # Need to create a .nojekyll file to allow filenames starting with an underscore
+    # to be seen on the gh-pages site. Therefore creating an empty .nojekyll file.
+    # Presumably this is only needed when the SHORT_NAMES option in Doxygen is set
+    # to NO, which it is by default. So creating the file just in case.
+    echo "" > .nojekyll
 
-            ################################################################################
-            ##### Generate the Doxygen code documentation and log the output.          #####
-            echo 'Generating Doxygen code documentation...'
-            # Redirect both stderr and stdout to the log file AND the console.
-            cd $TRAVIS_BUILD_DIR
-            doxygen $DOXYFILE 2>&1 | tee code_docs/doxygen.log
-            cd code_docs
+    ################################################################################
+    ##### Generate the Doxygen code documentation and log the output.          #####
+    echo 'Generating Doxygen code documentation...'
+    # Redirect both stderr and stdout to the log file AND the console.
+    cd $TRAVIS_BUILD_DIR
+    doxygen $DOXYFILE 2>&1 | tee code_docs/doxygen.log
+    cd code_docs
 
-            ################################################################################
-            ##### Upload the documentation to the gh-pages branch of the repository.   #####
-            # Only upload if Doxygen successfully created the documentation.
-            # Check this by verifying that the html directory and the file html/index.html
-            # both exist. This is a good indication that Doxygen did it's work.
-            if [ -d "html" ] && [ -f "html/index.html" ]; then
-                echo 'Uploading documentation to the gh-pages branch...'
-                # Add everything in this directory (the Doxygen code documentation) to the
-                # gh-pages branch.
-                # GitHub is smart enough to know which files have changed and which files have
-                # stayed the same and will only update the changed files.
-                git add --all
+    ################################################################################
+    ##### Upload the documentation to the gh-pages branch of the repository.   #####
+    # Only upload if Doxygen successfully created the documentation.
+    # Check this by verifying that the html directory and the file html/index.html
+    # both exist. This is a good indication that Doxygen did it's work.
+    if [ -d "html" ] && [ -f "html/index.html" ]; then
+        echo 'Uploading documentation to the gh-pages branch...'
+        # Add everything in this directory (the Doxygen code documentation) to the
+        # gh-pages branch.
+        # GitHub is smart enough to know which files have changed and which files have
+        # stayed the same and will only update the changed files.
+        git add --all
 
-                # Commit the added files with a title and description containing the Travis CI
-                # build number and the GitHub commit reference that issued this build.
-                git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+        # Commit the added files with a title and description containing the Travis CI
+        # build number and the GitHub commit reference that issued this build.
+        git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
 
-                # Force push to the remote gh-pages branch.
-                # The ouput is redirected to /dev/null to hide any sensitive credential data
-                # that might otherwise be exposed.
-                git push --force "git@github.com:$GH_REPO_ORG/$GH_REPO_NAME.git" > /dev/null 2>&1
-            else
-                echo '' >&2
-                echo 'Warning: No documentation (html) files have been found!' >&2
-                echo 'Warning: Not going to push the documentation to GitHub!' >&2
-                exit 1
-            fi
-        fi
+        # Force push to the remote gh-pages branch.
+        # The ouput is redirected to /dev/null to hide any sensitive credential data
+        # that might otherwise be exposed.
+        git push --force "git@github.com:$GH_REPO_ORG/$GH_REPO_NAME.git" > /dev/null 2>&1
+    else
+        echo '' >&2
+        echo 'Warning: No documentation (html) files have been found!' >&2
+        echo 'Warning: Not going to push the documentation to GitHub!' >&2
+        exit 1
     fi
 fi
-
-
